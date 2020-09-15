7e5801b0e08d178aa5bb8b485f6e51e2af19321e
Vitaliy <theyamshikov@yandex.ru>
2020-09-15 11:14:27 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -4,4 +4,5 @@ return [
     'ZnBundle\\Notify\\Domain\\Interfaces\\Repositories\\SmsRepositoryInterface' => 'ZnBundle\\Notify\\Domain\\Repositories\\Dev\\SmsRepository',
     'ZnBundle\\Notify\\Domain\\Interfaces\\Repositories\\TestRepositoryInterface' => 'ZnBundle\\Notify\\Domain\\Repositories\\Dev\\TestRepository',
     'ZnBundle\Notify\Domain\Interfaces\Repositories\FlashRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Session\FlashRepository',
+    'ZnBundle\\Notify\\Domain\\Interfaces\\Services\\SmsServiceInterface' => 'ZnBundle\\Notify\\Domain\\Services\\SmsService',
 ];
