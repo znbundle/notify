756379352f3740722c64c5d4215603c2f4e0d8dc
Vitaliy <theyamshikov@yandex.ru>
2020-09-26 21:03:39 +0600

    upd

--- src/Domain/Enums/NotifyPermissionEnum.php
+++ src/Domain/Enums/NotifyPermissionEnum.php
@@ -2,14 +2,13 @@
 
 namespace ZnBundle\Notify\Domain\Enums;
 
-use ZnCore\Domain\Base\BaseEnum;
-
-class NotifyPermissionEnum extends BaseEnum
+class NotifyPermissionEnum
 {
 
     const TEST_READ = 'oNotifyTestRead';
 
-    public static function getLabels() {
+    public static function getLabels()
+    {
         return [
             self::TEST_READ => 'Список тестовых сообщений. Чтение',
         ];
