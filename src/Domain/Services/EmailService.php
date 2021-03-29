eff387a7fe65fe422dc214104481543da2d8525b
vitaliy <theyamshikov@yandex.ru>
2021-03-29 08:17:35 +0200

    upd

--- src/Domain/Services/EmailService.php
+++ src/Domain/Services/EmailService.php
@@ -2,6 +2,7 @@
 
 namespace ZnBundle\Notify\Domain\Services;
 
+use Psr\Container\ContainerInterface;
 use ZnBundle\Notify\Domain\Entities\EmailEntity;
 use ZnBundle\Notify\Domain\Enums\ChannelEnum;
 use ZnBundle\Notify\Domain\Interfaces\Repositories\EmailRepositoryInterface;
@@ -15,16 +16,18 @@ class EmailService implements EmailServiceInterface
 
     private $emailRepository;
     private $jobService;
+    private $container;
 
-    public function __construct(EmailRepositoryInterface $emailRepository, JobServiceInterface $jobService)
+    public function __construct(EmailRepositoryInterface $emailRepository, JobServiceInterface $jobService, ContainerInterface $container)
     {
         $this->emailRepository = $emailRepository;
         $this->jobService = $jobService;
+        $this->container = $container;
     }
 
     public function push(EmailEntity $emailEntity, $priority = PriorityEnum::NORMAL)
     {
-        $emailJob = new SendEmailJob($this);
+        $emailJob = new SendEmailJob($this->container);
         $emailJob->entity = $emailEntity;
         $pushResult = $this->jobService->push($emailJob, $priority, ChannelEnum::EMAIL);
     }
