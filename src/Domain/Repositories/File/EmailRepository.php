2e19de0db21b2e16652df7b75b0fb9fd9dcf9121
Vitaliy <theyamshikov@yandex.ru>
2021-05-06 20:34:24 +0600

    upd

--- src/Domain/Repositories/File/EmailRepository.php
+++ src/Domain/Repositories/File/EmailRepository.php
@@ -4,30 +4,40 @@ namespace ZnBundle\Notify\Domain\Repositories\File;
 
 use ZnBundle\Notify\Domain\Entities\EmailEntity;
 use ZnBundle\Notify\Domain\Interfaces\Repositories\EmailRepositoryInterface;
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
 use ZnCore\Domain\Base\Repositories\BaseFileCrudRepository;
 use ZnCore\Domain\Helpers\EntityHelper;
-use ZnLib\Telegram\Domain\Facades\Bot;
-use ZnSandbox\Sandbox\Casbin\Domain\Entities\RoleEntity;
 
 class EmailRepository extends BaseFileCrudRepository implements EmailRepositoryInterface
 {
+
+    public $limitItems = 3;
 
     public function getEntityClass(): string
     {
         return EmailEntity::class;
     }
 
-    public function fileName(): string
+    public function tableName(): string
     {
-        return __DIR__ . '/../../../../../../../var/file-db/notify_email.php';
+        return 'notify_email';
     }
 
     public function send(EmailEntity $emailEntity)
     {
         $items = $this->getItems();
-        $items = [
-            EntityHelper::toArray($emailEntity)
-        ];
+        $items[] = EntityHelper::toArray($emailEntity);
+        $count = count($items);
+        if($count > $this->limitItems) {
+            $items = array_slice($items,  $count - $this->limitItems, $this->limitItems);
+        }
         $this->setItems($items);
+    }
+
+    public function oneLast(): EmailEntity
+    {
+        $items = $this->getItems();
+        $message = ArrayHelper::last($items);
+        return $this->getEntityManager()->createEntity($this->getEntityClass(), $message);
     }
 }
