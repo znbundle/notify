3d41eed00e1f2598bc3e98ae64ce7dd463908c07
Vitaliy <theyamshikov@yandex.ru>
2021-05-07 19:35:09 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,14 +1,22 @@
 <?php
 
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport\NullTransport;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use ZnCore\Base\Helpers\EnvHelper;
+
 return [
     'singletons' => [
+//        MailerInterface::class => Mailer::class,
+//        TransportInterface::class => NullTransport::class,
+        'ZnBundle\Notify\Domain\Interfaces\Repositories\EmailRepositoryInterface' => EnvHelper::isTest() ? 'ZnBundle\Notify\Domain\Repositories\File\EmailRepository' : 'ZnBundle\Notify\Domain\Repositories\Telegram\EmailRepository',
+        'ZnBundle\Notify\Domain\Interfaces\Repositories\SmsRepositoryInterface' => EnvHelper::isTest() ? 'ZnBundle\Notify\Domain\Repositories\File\SmsRepository' : 'ZnBundle\Notify\Domain\Repositories\Telegram\SmsRepository',
+
         'ZnBundle\Notify\Domain\Interfaces\Services\ToastrServiceInterface' => 'ZnBundle\Notify\Domain\Services\ToastrService',
         'ZnBundle\Notify\Domain\Interfaces\Services\FlashServiceInterface' => 'ZnBundle\Notify\Domain\Services\FlashService',
         'ZnBundle\Notify\Domain\Interfaces\Services\SmsServiceInterface' => 'ZnBundle\Notify\Domain\Services\SmsService',
         'ZnBundle\Notify\Domain\Interfaces\Services\EmailServiceInterface' => 'ZnBundle\Notify\Domain\Services\EmailService',
-        'ZnBundle\Notify\Domain\Interfaces\Repositories\EmailRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Telegram\EmailRepository',
-//        'ZnBundle\Notify\Domain\Interfaces\Repositories\SmsRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Dev\SmsRepository',
-//        'ZnBundle\Notify\Domain\Interfaces\Repositories\TestRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Dev\TestRepository',
         'ZnBundle\Notify\Domain\Interfaces\Repositories\FlashRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Session\FlashRepository',
         //'ZnBundle\Notify\Domain\Interfaces\Repositories\ToastrRepositoryInterface' => 'ZnBundle\Notify\Domain\Repositories\Session\ToastrRepository',
     ],
