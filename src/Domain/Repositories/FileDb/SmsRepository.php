eaf5b232a2c06b54548e880e729fd14ead2a5112
Vitaliy <theyamshikov@yandex.ru>
2021-05-07 11:40:18 +0600

    upd

---
+++ src/Domain/Repositories/FileDb/SmsRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ZnBundle\Notify\Domain\Repositories\FileDb;
+
+use ZnBundle\Notify\Domain\Entities\SmsEntity;
+use ZnBundle\Notify\Domain\Interfaces\Repositories\SmsRepositoryInterface;
+
+class SmsRepository extends BaseRepository implements SmsRepositoryInterface
+{
+
+    public function tableName(): string
+    {
+        return 'notify_sms';
+    }
+
+    public function getEntityClass(): string
+    {
+        return SmsEntity::class;
+    }
+
+    public function send(SmsEntity $smsEntity)
+    {
+        $this->insert($smsEntity);
+    }
+}
