dcb4d58482e59529f52950df81aae4f135c5aa94
Vitaliy <theyamshikov@yandex.ru>
2022-05-02 21:01:25 +0600

    upd

--- src/Domain/Enums/NotifyPermissionEnum.php
+++ src/Domain/Enums/NotifyPermissionEnum.php
@@ -2,7 +2,7 @@
 
 namespace ZnBundle\Notify\Domain\Enums;
 
-use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Contract\Enum\Interfaces\GetLabelsInterface;
 
 class NotifyPermissionEnum implements GetLabelsInterface
 {
