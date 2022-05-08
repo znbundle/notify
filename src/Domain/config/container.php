f8c0302d6b9135f76a821c2c81397514b64c4ed9
Vitaliy <theyamshikov@yandex.ru>
2022-05-08 20:22:28 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -4,7 +4,7 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport\NullTransport;
 use Symfony\Component\Mailer\Transport\TransportInterface;
-use ZnCore\Base\Helpers\EnvHelper;
+use ZnCore\Base\Libs\App\Helpers\EnvHelper;
 
 return [
     'singletons' => [
