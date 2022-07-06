07800dce65c171701c424ce8f27e9aafdde5b301
Vitaliy <theyamshikov@yandex.ru>
2022-07-06 18:44:34 +0600

    upd

--- src/Domain/Repositories/Null/SmsRepository.php
+++ src/Domain/Repositories/Null/SmsRepository.php
@@ -5,7 +5,7 @@ namespace ZnBundle\Notify\Domain\Repositories\Null;
 use ZnBundle\Notify\Domain\Entities\SmsEntity;
 use ZnBundle\Notify\Domain\Interfaces\Repositories\SmsRepositoryInterface;
 use ZnCore\Repository\Base\BaseRepository;
-use ZnCore\Domain\Domain\Interfaces\GetEntityClassInterface;
+use ZnCore\Domain\Interfaces\GetEntityClassInterface;
 
 class SmsRepository extends BaseRepository implements SmsRepositoryInterface, GetEntityClassInterface
 {
